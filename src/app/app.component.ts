20ffd158bb773d08f872a65f84482d2cbff2c104
zeneina <ina_zeneina@hotmail.com>
2024-04-03 13:41:16 +0300

    03-component-input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,31 @@
 import { Component } from '@angular/core';
 import { PersonTableComponent } from './components/person-table/person-table.component';
-
+import { Person } from './shared/interfaces/person';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [PersonTableComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   name = 'Ina';
 
+  person0 = 
+  {
+    givenName: 'Ina',
+    surName: 'Zenelaj',
+    age: 0x22,
+    email: 'ina_zeneina@aueb.gr',
+    address: 'Athens, Greece',
+  };
+  person1 = 
+  {
+    givenName: 'John',
+    surName: 'Doe',
+    age: 0x25,
+    email: 'john@example.com',
+    address: 'New York, USA',
+  };
 }
--- src/app/components/person-table/person-table.component.ts
+++ src/app/components/person-table/person-table.component.ts
@@ -1,18 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Person } from 'src/app/shared/interfaces/person';
 
 @Component({
   selector: 'app-person-table',
   standalone: true,
   imports: [],
   templateUrl: './person-table.component.html',
-  styleUrl: './person-table.component.css'
+  styleUrl: './person-table.component.css',
 })
 export class PersonTableComponent {
-
-  person  ={
-    givenName: 'Ina',
-    surName: 'Zenelaj',    
-    age: 0x20,
-    email:'ina@aueb.gr'
-    };
+  @Input() person: Person | undefined;
 }
