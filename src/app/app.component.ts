359d6ecbafd55bd4c1feab6dfd7f23accb8ff7e3
zeneina <ina_zeneina@hotmail.com>
2024-04-07 18:17:10 +0300

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,103 +1,19 @@
 import { Component } from '@angular/core';
 import { PersonTableComponent } from './components/person-table/person-table.component';
 import { Person } from './shared/interfaces/person';
+import { EventBindExampleComponent } from './components/evente-bind-example/evente-bind-example.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PersonTableComponent],
+  imports: [
+    RouterLink,
+    RouterOutlet,
+    PersonTableComponent,
+    EventBindExampleComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  name = 'Ina';
-
-  person0 = 
-  {
-    givenName: 'Ina',
-    surName: 'Zenelaj',
-    age: 0x20,
-    email: 'ina_zeneina@aueb.gr',
-    address: 'Athens, Greece',
-  };
-  person1 = 
-  {
-    givenName: 'John',
-    surName: 'Doe',
-    age: 0x25,
-    email: 'john@example.com',
-    address: 'New York, USA',
-  };
-  users: Person[] = [
-    {
-      givenName: 'John',
-      surName: 'Doe',
-      age: 30,
-      email: 'john.doe@example.com',
-      address: '123 Main St',
-    },
-    {
-      givenName: 'Jane',
-      surName: 'Doe',
-      age: 28,
-      email: 'jane.doe@example.com',
-      address: '123 Main St',
-    },
-    {
-      givenName: 'Jim',
-      surName: 'Brown',
-      age: 45,
-      email: 'jim.brown@example.com',
-      address: '456 Park Ave',
-    },
-    {
-      givenName: 'Jill',
-      surName: 'Brown',
-      age: 42,
-      email: 'jill.brown@example.com',
-      address: '456 Park Ave',
-    },
-    {
-      givenName: 'Jake',
-      surName: 'Smith',
-      age: 36,
-      email: 'jake.smith@example.com',
-      address: '789 Broadway',
-    },
-    {
-      givenName: 'Judy',
-      surName: 'Smith',
-      age: 34,
-      email: 'judy.smith@example.com',
-      address: '789 Broadway',
-    },
-    {
-      givenName: 'Jack',
-      surName: 'Johnson',
-      age: 50,
-      email: 'jack.johnson@example.com',
-      address: '321 Oak St',
-    },
-    {
-      givenName: 'Julie',
-      surName: 'Johnson',
-      age: 48,
-      email: 'julie.johnson@example.com',
-      address: '321 Oak St',
-    },
-    {
-      givenName: 'Jerry',
-      surName: 'Davis',
-      age: 55,
-      email: 'jerry.davis@example.com',
-      address: '654 Pine St',
-    },
-    {
-      givenName: 'June',
-      surName: 'Davis',
-      age: 53,
-      email: 'june.davis@example.com',
-      address: '654 Pine St',
-    },
-  ];
-}
+export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { EventBindExampleComponent } from './components/evente-bind-example/evente-bind-example.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'event-bind-example', component: EventBindExampleComponent },
+  { path: 'welcome', component: WelcomeComponent },
+  { path: '', redirectTo: '/welcome', pathMatch: 'full'}
+];
