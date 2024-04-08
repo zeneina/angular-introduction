2425935fcc7a8040a342fb763756e6cc13d55c66
zeneina <ina_zeneina@hotmail.com>
2024-04-08 16:19:42 +0300

    routing finished

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,17 @@
 import { Routes } from '@angular/router'
 import { EventBindExampleComponent } from './components/event-bind-example/event-bind-example.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
+import { Component } from '@angular/core';
+import { ComponentInputExampleComponent } from './components/component-input-example/component-input-example.component';
+import { ForDirectiveExampleComponent } from './components/for-directive-example.component/for-directive-example.component';
 
 export const routes: Routes = [
+  { path: 'component-input-example', 
+  component: ComponentInputExampleComponent},
+  { 
+    path: 'for-directive-example',
+    component: ForDirectiveExampleComponent,
+  },
   { path: 'event-bind-example', component: EventBindExampleComponent },
   //{ path: 'welcome', component: WelcomeComponent },
   { path: '', component: WelcomeComponent},
